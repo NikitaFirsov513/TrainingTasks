57b0c3bd4d361c0ac137f8a6f040a31f733fcba1
Nikita Firsov <nfirsov513@gmail.com>
2023-02-09 14:18:05 +0300

    init custom-components proj

---
+++ custom-components/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import "normalize.css";
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='app__container'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
