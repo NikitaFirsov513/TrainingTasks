bda69701d694d5adc8ecc44553881376fe385df2
Nikita Firsov <nfirsov513@gmail.com>
2023-02-12 19:17:23 +0300

    add error logic for input

--- custom-components/src/components/Inputs/Inputs.tsx
+++ custom-components/src/components/Inputs/Inputs.tsx
@@ -1,27 +1,19 @@
-import './Inputs.scss'
-
+import { Input } from "./Input";
+import "./Inputs.scss";
 
 export const Inputs = () => {
-
-
-    return (
-        <div className='inputs'>
-            <h1>Inputs</h1>
-            <div className='inputs__container'>
-                <label htmlFor="">
-                    Label
-                    <span className='inputs__container-not-required'>(Not Required)</span>
-                    <span className='inputs__container-required'>*</span>
-                </label>
-                <input  type="text" placeholder='Placeholder'/>
-            </div>
-            <div className='inputs__container'>
-                <label htmlFor="">
-                    Label
-                    <span className='inputs__container-not-required'>(Not Required)</span>
-                    <span className='inputs__container-required'>*</span>
-                </label>
-                <input disabled type="text" placeholder='Placeholder'/>
-            </div>
-        </div>)
-}
+  return (
+    <div className="inputs">
+      <h1>Inputs</h1>
+      <Input isRequired={false} isErr={false} isActive={true} />
+      <Input isRequired={true} isErr={false} isActive={true} />
+      <Input isRequired={false} isErr={false} isActive={false} />
+      <Input
+        isRequired={true}
+        isErr={true}
+        isActive={true}
+        errMesssage={"Messsage"}
+      />
+    </div>
+  );
+};
